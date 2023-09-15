9b72b6cfae468fcebee6812a18a5f25ff4778f47
Zack Dowd <zdowd2796@gmail.com>
2023-09-15 10:53:28 -0600

    refactored character class

--- Character.h
+++ Character.h
@@ -3,9 +3,8 @@
 class Character
 {
 public:
-    Character();
+    Character(int winWidth, int winHeight);
     Vector2 getWorldPos() { return worldPos; }
-    void setScreenPos(int winWidth, int winHeight);
     void tick(float deltaTime);
     void undoMovement();
 
@@ -26,4 +25,5 @@ private:
     const int maxFrames{6};
     const float updateTime{1.f / 12.f};
     const float speed{4.f};
+    float scale{4.f};
 };
